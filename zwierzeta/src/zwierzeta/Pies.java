12404602fc0e9a00a2999cbd915451efab1d5c78
Piotr Izak <pizak@objectivity.co.uk>
2019-06-15 16:10:32 +0200

    Zmiany w klasie Pies

--- zwierzeta/src/zwierzeta/Pies.java
+++ zwierzeta/src/zwierzeta/Pies.java
@@ -1,5 +1,21 @@
 package zwierzeta;
 
 public class Pies {
-
+	private String imie;
+	private int wiek;
+	
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public int getWiek() {
+		return wiek;
+	}
+	public void setWiek(int wiek) {
+		this.wiek = wiek;
+	}
+	
+	
 }
