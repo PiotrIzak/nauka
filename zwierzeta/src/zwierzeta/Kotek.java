ab9f7a0dd2fcfcb627b06ae158bcbf5c998f9c9d
Katarina Stojkovic <katarina.stojkovic-izak@capgemini.com>
2019-06-15 16:11:42 +0200

    Pierwsza rewizja kotka

--- zwierzeta/src/zwierzeta/Kotek.java
+++ zwierzeta/src/zwierzeta/Kotek.java
@@ -1,5 +1,32 @@
 package zwierzeta;
 
 public class Kotek {
+	
+	private String imie;	
+	private String kolor;
+	
+	public String getImie() {
+		return imie;
+	}
+
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+
+	public String getKolor() {
+		return kolor;
+	}
+
+	public void setKolor(String kolor) {
+		this.kolor = kolor;
+	}
+
+
+	
+	public void miaucz() {
+		System.out.println("Miaucze");
+		
+
+	}
 
 }
